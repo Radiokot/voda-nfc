9932777e1caf25d3df17ed77147b2f41df6c7f1c
Oleg Koretsky <test.radiokot@gmail.com>
2021-11-14 01:40:54 +0200

    Add Rx error handler

---
+++ app/src/main/java/ua/com/radiokot/voda/App.kt
@@ -0,0 +1,46 @@
+package ua.com.radiokot.voda
+
+import android.app.Application
+import android.util.Log
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
+import java.io.IOException
+import java.net.SocketException
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        initRxErrorHandler()
+    }
+
+    private fun initRxErrorHandler() {
+        RxJavaPlugins.setErrorHandler {
+            var e = it
+            if (e is UndeliverableException) {
+                e = e.cause
+            }
+            if ((e is IOException) || (e is SocketException)) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return@setErrorHandler
+            }
+            if (e is InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return@setErrorHandler
+            }
+            if ((e is NullPointerException) || (e is IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().uncaughtExceptionHandler
+                    ?.uncaughtException(Thread.currentThread(), e)
+                return@setErrorHandler
+            }
+            if (e is IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().uncaughtExceptionHandler
+                    ?.uncaughtException(Thread.currentThread(), e)
+                return@setErrorHandler
+            }
+            Log.w("RxErrorHandler", "Undeliverable exception received", e)
+        }
+    }
+}
