eb6440816dfbbc4abbbc54245f7d5a4116ea02f4
Oleg Koretsky <test.radiokot@gmail.com>
2021-11-15 00:25:58 +0200

    Implement design partially

--- app/src/main/java/ua/com/radiokot/voda/features/reader/model/VodaCard.kt
+++ app/src/main/java/ua/com/radiokot/voda/features/reader/model/VodaCard.kt
@@ -6,4 +6,6 @@ data class VodaCard(
     val balance: Long,
 ) {
     val balanceUah: BigDecimal = BigDecimal(balance).movePointLeft(2)
+
+    fun getLiters(pricePerLiter: Int): Long = balance / pricePerLiter
 }
--- app/src/main/java/ua/com/radiokot/voda/features/reader/view/ReaderCardDataFragment.kt
+++ app/src/main/java/ua/com/radiokot/voda/features/reader/view/ReaderCardDataFragment.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.fragment_reader_card_data.*
 import ua.com.radiokot.voda.BaseFragment
 import ua.com.radiokot.voda.R
 import ua.com.radiokot.voda.features.reader.model.VodaCardsSource
+import java.math.BigDecimal
 
 class ReaderCardDataFragment: BaseFragment() {
     override fun onCreateView(
@@ -25,7 +26,8 @@ class ReaderCardDataFragment: BaseFragment() {
             .cards
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe { card ->
-                balance_text_view.text = card.balanceUah.toPlainString()
+                liters_text_view.text = BigDecimal(card.getLiters(140)).toPlainString()
+                liters_label_text_view.text = resources.getQuantityString(R.plurals.liters, (card.balance / 100).toInt())
             }
             .addTo(compositeDisposable)
     }
