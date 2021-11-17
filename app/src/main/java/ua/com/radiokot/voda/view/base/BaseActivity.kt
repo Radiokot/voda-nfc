d324b1f969ed1bb6792f69f8328a645aca1a193d
Oleg Koretsky <test.radiokot@gmail.com>
2021-11-17 15:39:41 +0200

    Add launcher icon and splash background

--- app/src/main/java/ua/com/radiokot/voda/view/base/BaseActivity.kt
+++ app/src/main/java/ua/com/radiokot/voda/view/base/BaseActivity.kt
@@ -1,8 +1,12 @@
 package ua.com.radiokot.voda.view.base
 
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import io.reactivex.disposables.CompositeDisposable
 import org.koin.android.ext.android.inject
+import ua.com.radiokot.voda.R
 import ua.com.radiokot.voda.view.ToastManager
 
 abstract class BaseActivity : AppCompatActivity() {
@@ -12,6 +16,14 @@ abstract class BaseActivity : AppCompatActivity() {
     protected val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     protected val toastManager: ToastManager by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        window.setBackgroundDrawable(
+            ColorDrawable(ContextCompat.getColor(this, R.color.background))
+        )
+    }
 
     override fun onDestroy() {
         super.onDestroy()
