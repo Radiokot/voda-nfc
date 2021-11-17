c1a21c439f1e7ae0b3b82fc62045af7268634fa0
Oleg Koretsky <test.radiokot@gmail.com>
2021-11-17 14:41:41 +0200

    Add a separate activity for NFC intent handling

---
+++ app/src/main/java/ua/com/radiokot/voda/features/card/view/SingleCardReadActivity.kt
@@ -0,0 +1,87 @@
+package ua.com.radiokot.voda.features.card.view
+
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.os.Bundle
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
+import org.koin.android.ext.android.inject
+import org.koin.core.parameter.parametersOf
+import ua.com.radiokot.voda.R
+import ua.com.radiokot.voda.features.card.model.VodaCard
+import ua.com.radiokot.voda.features.reader.logic.VodaCardReader
+import ua.com.radiokot.voda.view.base.BaseActivity
+
+class SingleCardReadActivity : BaseActivity(), VodaCardsSource {
+    private val tagsSubject: PublishSubject<Tag> = PublishSubject.create()
+    private val cardReader: VodaCardReader by inject { parametersOf(tagsSubject) }
+
+    private val cardsSubject: BehaviorSubject<VodaCard> = BehaviorSubject.create()
+    override val cards: Observable<VodaCard> = cardsSubject
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_card_read)
+
+        initReader()
+
+        if (savedInstanceState == null && NfcAdapter.ACTION_TECH_DISCOVERED == intent.action) {
+            val intentTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG) as? Tag
+            if (intentTag != null) {
+                tagsSubject.onNext(intentTag)
+            } else {
+                finish()
+            }
+        } else {
+            val savedCard = savedInstanceState?.getSerializable(CARD_DATA_STATE_KEY) as? VodaCard
+            if (savedCard != null) {
+                cardsSubject.onNext(savedCard)
+            } else {
+                finish()
+            }
+        }
+    }
+
+    private fun initReader() {
+        cardReader
+            .cards
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy(
+                onNext = cardsSubject::onNext,
+                onError = { fatalError ->
+                    fatalError.printStackTrace()
+                    toastManager.short(
+                        getString(
+                            R.string.template_error_fatal_message,
+                            fatalError.localizedMessage ?: fatalError.message
+                        )
+                    )
+                    finish()
+                }
+            )
+            .addTo(compositeDisposable)
+
+        cardReader
+            .errors
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { error ->
+                error.printStackTrace()
+                toastManager.short(R.string.error_failed_to_read_card_try_again)
+                finish()
+            }
+            .addTo(compositeDisposable)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(CARD_DATA_STATE_KEY, cardsSubject.value)
+    }
+
+    private companion object {
+        private const val CARD_DATA_STATE_KEY = "card"
+    }
+}
--- app/src/main/java/ua/com/radiokot/voda/features/reader/view/ReaderActivity.kt
+++ app/src/main/java/ua/com/radiokot/voda/features/reader/view/ReaderActivity.kt
@@ -1,23 +1,20 @@
 package ua.com.radiokot.voda.features.reader.view
 
-import android.nfc.NfcAdapter
-import android.nfc.Tag
 import android.os.Bundle
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.addTo
 import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.subjects.BehaviorSubject
-import io.reactivex.subjects.PublishSubject
 import kotlinx.android.synthetic.main.activity_reader.*
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
 import ua.com.radiokot.voda.R
 import ua.com.radiokot.voda.extensions.moderateSmoothScrollToPosition
+import ua.com.radiokot.voda.features.card.model.VodaCard
+import ua.com.radiokot.voda.features.card.view.VodaCardsSource
 import ua.com.radiokot.voda.features.nfc.logic.NfcReader
 import ua.com.radiokot.voda.features.nfc.logic.SimpleNfcReader
 import ua.com.radiokot.voda.features.reader.logic.VodaCardReader
-import ua.com.radiokot.voda.features.card.model.VodaCard
-import ua.com.radiokot.voda.features.card.view.VodaCardsSource
 import ua.com.radiokot.voda.view.base.BaseActivity
 
 
@@ -26,13 +23,7 @@ class ReaderActivity : BaseActivity(), VodaCardsSource {
         SimpleNfcReader(this)
     }
 
-    private val tagsSubject: PublishSubject<Tag> by lazy {
-        PublishSubject
-            .create<Tag>()
-            .also(nfcReader.tags::subscribe)
-    }
-
-    private val cardReader: VodaCardReader by inject { parametersOf(tagsSubject) }
+    private val cardReader: VodaCardReader by inject { parametersOf(nfcReader.tags) }
 
     private val fragmentsAdapter = ReaderFragmentsAdapter(supportFragmentManager, lifecycle)
 
@@ -50,13 +41,6 @@ class ReaderActivity : BaseActivity(), VodaCardsSource {
             if (it is VodaCard) {
                 cardsSubject.onNext(it)
             }
-        }
-
-        if (NfcAdapter.ACTION_TECH_DISCOVERED == intent.action) {
-            intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-                ?.also { intentTag ->
-                    tagsSubject.onNext(intentTag)
-                }
         }
     }
 
